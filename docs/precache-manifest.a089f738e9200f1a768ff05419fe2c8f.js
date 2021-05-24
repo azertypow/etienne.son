2b1d4e4369b003470de1c61aaa0497b3b58a9c03
azertypow <azertypow@gmail.com>
2021-05-24 13:27:08 +0200

    build v4

---
+++ docs/precache-manifest.a089f738e9200f1a768ff05419fe2c8f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea845f098bf7e9ea070254b9681853a",
+    "url": "https://azertypow.github.io/etienne.son/1Minencial_Gym_Mix.wav"
+  },
+  {
+    "revision": "e8ae95b9655c52a1e0224a1505627737",
+    "url": "https://azertypow.github.io/etienne.son/2Minencial_Blade_Mix.wav"
+  },
+  {
+    "revision": "a9b9881b3969b4dfff7f4426ca9f8cbd",
+    "url": "https://azertypow.github.io/etienne.son/3Minencial_Mijaba_Mix.wav"
+  },
+  {
+    "revision": "b8a8ea84080db2c6be55",
+    "url": "https://azertypow.github.io/etienne.son/css/app.59a84fd6.css"
+  },
+  {
+    "revision": "52bc1d7c6e40207d68b6184b4f1afe74",
+    "url": "https://azertypow.github.io/etienne.son/img/APPS2_SOUND@2x.52bc1d7c.png"
+  },
+  {
+    "revision": "31cec3ef75c153c300d5b38b17b9df4b",
+    "url": "https://azertypow.github.io/etienne.son/index.html"
+  },
+  {
+    "revision": "b8a8ea84080db2c6be55",
+    "url": "https://azertypow.github.io/etienne.son/js/app.97e15687.js"
+  },
+  {
+    "revision": "2a24952a225f4e6fb5ee",
+    "url": "https://azertypow.github.io/etienne.son/js/chunk-vendors.05f38160.js"
+  },
+  {
+    "revision": "ad64b18fd4ea1b02a1205eac04470b9e",
+    "url": "https://azertypow.github.io/etienne.son/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://azertypow.github.io/etienne.son/robots.txt"
+  }
+]);
